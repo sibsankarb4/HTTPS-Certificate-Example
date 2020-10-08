474ea6e19a2c7fbe8b86ba0f595fb81ad9265160
Sibsankar Bera <sibsankarb4@gmail.com>
2020-10-08 21:13:45 +0530

    Add files via upload

---
+++ src/main/java/com/https/demo/HttpsCertificateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.https.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsCertificateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsCertificateExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/https/demo/HttpsCertificateExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.https.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsCertificateExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
